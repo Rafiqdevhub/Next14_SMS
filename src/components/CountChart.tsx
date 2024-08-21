5062a1bf2403e26d3dbad81dd5fe77de707960b2
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-08-21 22:27:48 +0500

    Update chart labels and titles for gender distribution

--- src/components/CountChart.tsx
+++ src/components/CountChart.tsx
@@ -14,12 +14,12 @@ const data = [
     fill: "white",
   },
   {
-    name: "Girls",
+    name: "Ladies",
     count: 53,
     fill: "#FAE27C",
   },
   {
-    name: "Boys",
+    name: "Gentlemen",
     count: 53,
     fill: "#C3EBFA",
   },
@@ -28,12 +28,10 @@ const data = [
 const CountChart = () => {
   return (
     <div className="bg-white rounded-xl w-full h-full p-4">
-      {/* TITLE */}
       <div className="flex justify-between items-center">
         <h1 className="text-lg font-semibold">Students</h1>
         <Image src="/moreDark.png" alt="" width={20} height={20} />
       </div>
-      {/* CHART */}
       <div className="relative w-full h-[75%]">
         <ResponsiveContainer>
           <RadialBarChart
@@ -55,17 +53,16 @@ const CountChart = () => {
           className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
         />
       </div>
-      {/* BOTTOM */}
       <div className="flex justify-center gap-16">
         <div className="flex flex-col gap-1">
           <div className="w-5 h-5 bg-lamaSky rounded-full" />
           <h1 className="font-bold">1,234</h1>
-          <h2 className="text-xs text-gray-300">Boys (55%)</h2>
+          <h2 className="text-xs text-gray-300">Gentlemen (55%)</h2>
         </div>
         <div className="flex flex-col gap-1">
           <div className="w-5 h-5 bg-lamaYellow rounded-full" />
           <h1 className="font-bold">1,234</h1>
-          <h2 className="text-xs text-gray-300">Girls (45%)</h2>
+          <h2 className="text-xs text-gray-300">Ladies (45%)</h2>
         </div>
       </div>
     </div>
